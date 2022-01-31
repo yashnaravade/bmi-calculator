dd95410bb90d9dcf8ad0342c57f1605cef67dad5
surajshende247 <surajshende247@gmail.com>
2022-01-31 20:34:06 +0530

    remark for BMI added

--- js/script.js
+++ js/script.js
@@ -7,4 +7,18 @@ function calculateBMI() {
   var bmi = (weight / (heightM * heightM)).toFixed(2);
 
   document.getElementById("txt-bmi").innerHTML ="BMI: "+ bmi;
+
+  if(bmi < 18.5) {
+    document.getElementById("txt-remark").innerHTML = "Underweight";
+  }
+  else if(bmi >= 18.5 && bmi <= 24.9) {
+    document.getElementById("txt-remark").innerHTML = "Normal";
+  }
+  else if(bmi >= 25 && bmi <= 29.9) {
+    document.getElementById("txt-remark").innerHTML = "Overweight";
+  }
+  else if(bmi >= 30) {
+    document.getElementById("txt-remark").innerHTML = "Obese";
+  }
+
 } 
